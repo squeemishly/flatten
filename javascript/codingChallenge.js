a14acbb7cc974e06b43e1ed0084a84954a54392a
squeemishly <squeemishly@gmail.com>
2017-10-04 15:26:04 -0600

    flattens a doubly nested array

--- javascript/codingChallenge.js
+++ javascript/codingChallenge.js
@@ -1,6 +1,16 @@
 class CustomArray {
   flatten(arr) {
-    return arr
+    let builder = []
+    arr.forEach( item => {
+      if (item instanceof Array) {
+        item.forEach( insideItem => {
+          builder.push(insideItem)
+        })
+      } else {
+        builder.push(item)
+      }
+    })
+    return builder
   }
 }
 
