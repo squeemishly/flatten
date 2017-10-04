e9682cd396faf5b29fb43463b2bf730d8f981a78
squeemishly <squeemishly@gmail.com>
2017-10-03 19:49:58 -0600

    ugly solution for a singly nested array

--- ruby/coding_challenge.rb
+++ ruby/coding_challenge.rb
@@ -2,6 +2,14 @@ require 'pry'
 
 class CustomArray
   def flatten(arr)
-    arr
+    arr.reduce([]) do |final_arr, item|
+      if item.class == Array
+        item.each do |a_single_item|
+          final_arr << a_single_item
+        end
+      else
+        final_arr << item
+      end
+    end
   end
 end
