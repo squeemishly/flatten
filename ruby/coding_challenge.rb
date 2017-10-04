f958b78a20f4909adfe70d67bc3f9443c1e0bfa1
squeemishly <squeemishly@gmail.com>
2017-10-04 15:02:13 -0600

    moves conditional logic to their own methods, creates private section for methods used in flatten

--- ruby/coding_challenge.rb
+++ ruby/coding_challenge.rb
@@ -3,16 +3,26 @@ require 'pry'
 class CustomArray
   def flatten(arr)
     arr.reduce([]) do |final_arr, item|
-      if array?(item)
-        item.each { |a_single_item| final_arr << a_single_item }
-      else
-        final_arr << item
-      end
-      if reflattenify(final_arr)
-        flatten(final_arr)
-      else
-        final_arr
-      end
+      create_final_array(final_arr, item)
+      recurse?(final_arr)
+    end
+  end
+
+  private
+
+  def recurse?(final_arr)
+    if reflattenify(final_arr)
+      flatten(final_arr)
+    else
+      final_arr
+    end
+  end
+
+  def create_final_array(final_arr, item)
+    if array?(item)
+      item.each { |a_single_item| final_arr << a_single_item }
+    else
+      final_arr << item
     end
   end
 
